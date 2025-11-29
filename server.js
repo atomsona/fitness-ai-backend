fc9a63e12e4e5e711d09d5ef3210ab3815a7443a
gio <giozedelashvili@gmail.com>
2025-11-29 14:47:28 +0400

    update

--- server.js
+++ server.js
@@ -7,23 +7,15 @@ import authRoutes from './routes/auth.js';
 import userRoutes from './routes/user.js';
 import questRoutes from './routes/quests.js';
 import adminRoutes from './routes/admin.js';
-import serverless from 'serverless-http';
 
 dotenv.config();
 const app = express();
 
+// CORS
+console.log(process.env.CLIENT_URL, "client URL")
+app.use(cors({ origin: [process.env.CLIENT_URL], credentials: true }));
 
-
-// Required for Vercel preflight
-app.options(
-  '*',
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
-
-// Body + cookies
+// Body & cookie
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -34,13 +26,19 @@ app.use('/api/user', userRoutes);
 app.use('/api/quests', questRoutes);
 app.use('/api/admin', adminRoutes);
 
-// Root test
-app.get('/', (req, res) => {
-  res.json({ message: 'Fitness AI Backend Running' });
-});
+// Root
+app.get('/', (req, res) => res.json({ message: 'Fitness AI Backend Running' }));
 
-// Connect database ONCE (not per request)
-await connectDB();
+// 404
+app.use((req, res) => res.status(404).json({ success: false, message: 'Route not found', path: req.path }));
 
-// Export serverless handler for Vercel
-export const handler = serverless(app);
+// Error handler
+app.use((err, req, res, next) => res.status(err.status || 500).json({ success: false, message: err.message || 'Internal Server Error' }));
+
+connectDB().then((res) => {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+})
+
+
